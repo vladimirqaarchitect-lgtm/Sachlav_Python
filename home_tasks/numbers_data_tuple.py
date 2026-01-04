06718a02b95c6f567efab3afdc3e42baba50e0b6
vladimirqaarchitect-lgtm <vladimir.qa.architect@gmail.com>
2026-01-04 20:26:49 +0200

    add nambers_data_tuple

---
+++ home_tasks/numbers_data_tuple.py
@@ -0,0 +1,16 @@
+number_data = [15, 22, 10, 30, 5, 25, 18]
+
+number_data_sum = sum(number_data)
+
+number_data_avr = sum(number_data) / len(number_data)
+
+number_data_min = min(number_data)
+
+number_data_max = max(number_data)
+
+
+
+print(number_data_sum)
+print(number_data_avr)
+print(number_data_min)
+print(number_data_max)
