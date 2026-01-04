68445fd3a17c878d7e993608eda4ab29efb32909
vladimirqaarchitect-lgtm <vladimir.qa.architect@gmail.com>
2026-01-04 20:56:04 +0200

    add shopping_list

---
+++ home_tasks/shopping_list.py
@@ -0,0 +1,27 @@
+my_shopping_list = ["milk", "bread", "eggs", " cheese", "butter"]
+
+my_shopping_list.append("juice")
+print(my_shopping_list)
+
+my_shopping_list.insert(1, "joghurt")
+print(my_shopping_list)
+
+my_shopping_list[2] = "loaf"
+print(my_shopping_list)
+
+my_shopping_list.remove("butter")
+print(my_shopping_list)
+
+my_shopping_list.sort()
+print(my_shopping_list)
+
+print("Первый элемент:", my_shopping_list[0])
+print("Последний элемент:", my_shopping_list[-1])
+
+print(my_shopping_list[1:4])
+
+my_shopping_list.sort()
+print(my_shopping_list)
+
+print(my_shopping_list)
+print(len(my_shopping_list))
